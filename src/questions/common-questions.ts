a89fe943d112f23450210179ea84db11e79b1f4f
Sebastian Quintero <sebastianquintero@habi.co>
2024-01-09 10:23:01 -0500

    [UPD]Second case added

--- src/questions/common-questions.ts
+++ src/questions/common-questions.ts
@@ -4,3 +4,9 @@ export const titleResultVisible = () =>
     Text.of(
         PageElement.located(By.xpath('//*[@id="firstHeading"]/span'))
     ).describedAs('Título del artículo')
+
+
+export const titleResultNotVisible = () =>
+    Text.of(
+        PageElement.located(By.xpath('//*[@id="mw-content-text"]/div[4]/div[1]/p'))
+    ).describedAs('Sugerencia de creación de artículo')
